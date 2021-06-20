e50bd70039b441c7ab40d17c15028c80ef2180fe
Risky Solihin <santuyaja019@gmail.com>
2021-06-20 22:36:06 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Server
+#### Informasih
+* **Server Ini Khusus Premium !!**
+* **Terima Kasih Kepada !!**
+* **Dapunta,Yayan,Angga,Hamzah,Ratu Error !!**
+
+#### Kontak !!
+
+* 6283143565470
---
+++ Upload.sh
@@ -0,0 +1,11 @@
+git init 
+git add *
+git commit -m "first commit" 
+git branch -M main 
+git remote add origin https://github.com/Dumai-991/Server.git
+git push -u origin main
+
+
+
+#Cieee Anak Ajg
+#Conding-utf-bash
